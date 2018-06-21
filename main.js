1b2a80586477e4018885bdc632e706d2850c9647
Alan <alanwu9296@gmail.com>
2018-06-20 22:17:13 -0400

    model component

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
+ import Vue from 'vue'
 // import Markdown from './markdown.vue'
 //import Github from './github.vue'
 //import GridComponent from './gridComponent'
 //import TreeView from './TreeView/treeView.vue'
-
-import SvgView from './svgGraph/svgView.vue'
+//import SvgView from './svgGraph/svgView.vue'
+import modalView from './modal/modalView.vue'
 
 new Vue({
     el:"#app",
-    render:h=>h(SvgView)
+    render:h=>h(modalView)
 })
---
+++ modal/modalView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button @click="showModal = true">Show Modal</button>
+        <modal v-if="showModal" @close="showModal = false">
+            <h3 slot="header">Custom Header</h3>
+        </modal>
+    </div>
+</template>
+
+<script>
+import modal from "./modal.vue"
+
+    export default {
+        name:"modalView",
+        data(){return{
+            showModal:false
+        }},
+        components:{
+            modal
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
